ab4a1a41d751ede134d4a540ee5ab5de8fa50d8c
Evan Gudmestad <eagudmestad@ranken.edu>
2015-10-23 10:49:57 -0500

    First Commit to Concern Report SYstem

---
+++ ConcernReportSystem/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace ConcernReportSystem.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ ConcernReportSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConcernReportSystem.Global" Language="C#" %>
